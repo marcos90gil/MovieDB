0530ccaa06348b7d5aaf32bddc4a4e77fe7c0be5
Marcos Gil <marcos_gilf@hotmail.com>
2016-03-22 11:55:39 +0100

    Añadimos variable de UI STACK para ordenar la interfaz y lo añadimos la vista de películas

--- scripts/controllers/MoviesListController.js
+++ scripts/controllers/MoviesListController.js
@@ -1,6 +1,22 @@
 angular.module("moviedb").controller("MoviesListController", 
-	["$scope", function($scope){
-		$scope.name = "Joseba";
+	["$scope", "MovieService", function($scope, MovieService){
+		
+		// Scope model init
+		$scope.uiState = 'blank';
+		$scope.model = [];
+
+        // Scope watchers
+        $scope.$watch("model", function(newValue, oldValue){
+        	if (newValue.length === 0) {
+        		$scope.uiState = "blank";
+        	} else {
+        		$scope.uiState = "ideal";
+        	}
+        });
+
+        // Controller start
+        $scope.model = MovieService.getMovies();
+
 	}]
 
 );
